6c25bb0b2d95b769fd1dee3264fbbb49f36a1f52
Den Kom <deniss.komarskis@gmail.com>
2025-03-12 00:06:50 +0200

    impr: improved options usage in code, skip renaming if name is already alright

--- rename-files.rb
+++ rename-files.rb
@@ -4,35 +4,85 @@ require 'mini_exiftool'
 require 'fileutils'
 require 'optparse'
 
-DEFAULT_DATETIME_FORMAT = "%Y%m%d-%H.%M.%S"
+DEFAULT_DATETIME_FORMAT = "%Y-%m-%d-%H.%M.%S"
 
-def rename_files_in_directory(directory_path, datetime_format, custom_file_prefix)
-  return p "Directory does not exist! #{directory_path}" unless Dir.exist?(directory_path)
+OptionsStruct = Struct.new(:directory_path, :custom_format, :custom_file_prefix)
 
-  Dir.foreach(directory_path) do |file|
-    next if file == '.' || file == '..' # Skip special entries
-    
-    current_file_path = File.join(directory_path, file)
-    next unless File.file?(current_file_path)
+def main()
+  options = OptionsStruct.new
 
-    rename_file(file, current_file_path, custom_file_prefix, datetime_format)
+  OptionParser.new do |parser|
+    parser.on("-d", "--directory DIRECTORY", "Directory path is required") do |d|
+      options.directory_path = d
+    end
+    parser.on("-f", "--format FORMAT", "Provide custom format") do |f|
+      options.custom_format = f
+    end
+    parser.on("-c", "--custom CUSTOM", "Set custom file prefix") do |c|
+      options.custom_file_prefix = c
+    end
+  end.parse!
+
+  unless options.directory_path
+    p "Missing directory_path argument"
+    return
+  end
+
+  options.custom_format ||= DEFAULT_DATETIME_FORMAT
+  unless valid_strftime_format?(options.custom_format)
+    p "Invalid custom format"
+    return
+  end
+
+  rename_files_in_directory(options)
+end
+
+def valid_strftime_format?(custom_format)
+  begin
+    Time.now.strftime(custom_format)
+    true
+  rescue ArgumentError
+    false
   end
 end
 
-def rename_file(file, current_file_path, custom_file_prefix, datetime_format)
-  formated_creation_time = get_formated_creation_time(current_file_path, datetime_format)
+def rename_files_in_directory(options)
+  return p "Directory does not exist! #{options.directory_path}" unless Dir.exist?(options.directory_path)
+
+  Dir.foreach(options.directory_path) do |file|
+    next if file == '.' || file == '..' # Skip special entries
+
+    current_file_path = File.join(options.directory_path, file)
+    next unless File.file?(current_file_path)
+
+    rename_file(file, current_file_path, options)
+  end
+end
+
+def rename_file(file, current_file_path, options)
+  custom_file_prefix = options.custom_file_prefix ? "#{options.custom_file_prefix}-" : ""
+  formated_creation_time = get_formated_creation_time(current_file_path, options.custom_format)
   extension = File.extname(file)
   current_directory = File.dirname(current_file_path)
 
-  custom_file_prefix += "-" if !custom_file_prefix.empty?
-
   new_name = "#{custom_file_prefix}#{formated_creation_time}#{extension}"
   new_file_path = File.join(current_directory, new_name)
+
+  if current_file_path == new_file_path
+    p "Keep current name: #{new_name}"
+    return
+  end
 
   i = 1
   while File.exist?(new_file_path)
     new_name = "#{custom_file_prefix}#{formated_creation_time}-#{i.to_s.rjust(3, '0')}#{extension}"
     new_file_path = File.join(current_directory, new_name)
+
+    if current_file_path == new_file_path
+      p "Keep current name: #{new_name}"
+      return
+    end
+
     i += 1
   end
 
@@ -58,42 +108,5 @@ def get_formated_creation_time(file_path, datetime_format)
   end
 end
 
-def valid_strftime_format?(custom_format)
-  begin
-    Time.now.strftime(custom_format)
-    true
-  rescue ArgumentError
-    false
-  end
-end
+main()
 
-options = {}
-OptionParser.new do |parser|
-  parser.on("-d", "--directory DIRECTORY", "Directory path is required") do |d|
-    options[:directory_path] = d
-  end
-  parser.on("-f", "--format FORMAT", "Provide custom format") do |f|
-    options[:custom_format] = f
-  end
-  parser.on("-c", "Set custom file prefix") do |c|
-    options[:custom_file_prefix] = c
-  end
-end.parse!
-
-unless options[:directory_path]
-  p "Missing directory_path argument"
-  return
-end
-
-options[:custom_format] ||= DEFAULT_DATETIME_FORMAT
-options[:custom_file_prefix] ||= ""
-
-if options[:custom_format]
-  p "Set custom format: #{options[:custom_format]}"
-  unless valid_strftime_format?(options[:custom_format])
-    p "Invalid custom format"
-    return
-  end
-end
-
-rename_files_in_directory(options[:directory_path], options[:custom_format], options[:custom_file_prefix])
